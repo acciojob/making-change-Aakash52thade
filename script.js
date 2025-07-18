d6eacdf9754de27664619ea425ccbbadde0a3af4
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-18 18:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const makeChange = (c) => {
+fjkdjsconst makeChange = (c) => {
   // your name here
-};
+	c = Number(c);
 
+	const change = [];
+
+	change.q = Math.floor(c / 25);
+	   c = c % 25;
+
+	change.d = Math.floor(c / 10);
+	c = c % 10;
+
+	change.d = Math.floor(c / 5);
+	c = c % 5;
+
+	change.p = c;
+
+	return change;
+};
+ 
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
